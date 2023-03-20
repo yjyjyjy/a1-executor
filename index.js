18ec7963005b1caba1360eb6fcbe260aa61640ee
yjyjyjy <prowessyang@gmail.com>
2023-03-20 13:50:50 -0700

    works if run consistently

--- index.js
+++ index.js
@@ -8,70 +8,97 @@ functions.http('sdrequest', async (req, res) => {
   console.log(`🪵:sdrequest:${JSON.stringify(req.body)}`)
   const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
   const ec2Client = new EC2Client({ region: "us-east-1" });
-  const { id, endpoint, params } = req.body
+  const { id, endpoint, sd_model_checkpoint, params } = req.body
 
   // endpoint
   // bring up the inference machine
 
   try {
-    const inferenceInstanceId = 'i-0af68d8e2fcef2b02'
-    let instanceStartCalled = false
-    let ip = ''
+    // const inferenceInstanceId = 'i-0586907b3f7ee3d10'
+    // let instanceStartCalled = false
+    // let ip = ''
+    // let sd_model_checkpoint
+    const instanceIp = '44.204.101.147'
 
-    const checkInferenceEC2Status = async () => {
-      const { InstanceStatuses } = await ec2Client.send(new DescribeInstanceStatusCommand({
-        InstanceIds: [inferenceInstanceId]
-      }))
-      const status = InstanceStatuses[0]?.InstanceState?.Name
-      if (status === 'stopping') {
-        setTimeout(await checkInferenceEC2Status(), 5000)
-      }
+    const updateOption = async () => {
 
-      let { data: selectedInstances, error: selectInstanceError } = await supabase
-        .from('instance')
-        .select('id, status, ip')
-        .eq('id', inferenceInstanceId)
-      if (selectInstanceError) { throw selectInstanceError }
-      if (selectedInstances.length === 1 && selectedInstances[0].status === 'running' && selectedInstances[0].ip) {
-        console.log('status:', status)
-        if (status === 'running') {
-          console.log('🔴 selectedInstances: ', selectedInstances)
-          ip = selectedInstances[0].ip
-          return
-        }
-        await supabase
-          .from('instance')
-          .update({ status })
-          .eq('id', inferenceInstanceId)
+      // let { data: selectedInstances, error: selectInstanceError } = await supabase
+      //   .from('instance')
+      //   .select('id, status, ip')
+      //   .eq('id', inferenceInstanceId)
+      // if (selectInstanceError) { throw selectInstanceError }
+      // if (selectedInstances.length === 1 && selectedInstances[0].status === 'running' && selectedInstances[0].ip) {
+      //   console.log('status:', status)
+      //   if (status === 'running') {
+      //     console.log('🔴 selectedInstances: ', selectedInstances)
+      //     ip = selectedInstances[0].ip
+      //     return
+      //   }
+      //   await supabase
+      //     .from('instance')
+      //     .update({ status })
+      //     .eq('id', inferenceInstanceId)
+      // }
+      const configApiUrl = `http://${instanceIp}:7860/sdapi/v1/options`
+      console.log('configApiUrl: ', configApiUrl)
+      const resp = await axios.post(
+        configApiUrl,
+        { sd_model_checkpoint },
+        { headers: { 'Content-Type': 'application/json' } }
+      )
+      console.log(`🪵:sdrequest:updateOption:${resp.status}`)
+      if (resp.status !== 200) {
+        return res.status(500).json({ message: 'inference node is down' })
       }
+      // console.log(options)
+      // if (options.sd_model_checkpoint) {
+      //   const model = options.sd_model_checkpoint.split('.ckpt')[0].split('.safetensors')[0]
+      //   if (mode) {
+      //     sd_model_checkpoint = model
+      //   }
+      // }
 
-      console.log('selectedInstances: ', JSON.stringify(selectedInstances))
-      if (!instanceStartCalled) {
-        const { StartingInstances } = await ec2Client.send(new StartInstancesCommand({
-          "InstanceIds": [inferenceInstanceId]
-        }))
-        console.log(`StartingInstances ${JSON.stringify(StartingInstances)}`)
-        instanceStartCalled = true
-      }
-      setTimeout(await checkInferenceEC2Status(), 5000)
+
+
+      // const { InstanceStatuses } = await ec2Client.send(new DescribeInstanceStatusCommand({
+      //   InstanceIds: [inferenceInstanceId]
+      // }))
+      // const status = InstanceStatuses[0]?.InstanceState?.Name
+      // if (status === 'stopping') {
+      //   setTimeout(await checkInferenceEC2Status(), 5000)
+      // }
+
+
+      // console.log('selectedInstances: ', JSON.stringify(selectedInstances))
+      // if (!instanceStartCalled) {
+      //   const { StartingInstances } = await ec2Client.send(new StartInstancesCommand({
+      //     "InstanceIds": [inferenceInstanceId]
+      //   }))
+      //   console.log(`StartingInstances ${JSON.stringify(StartingInstances)}`)
+      //   instanceStartCalled = true
+      // }
+      // setTimeout(await checkInferenceEC2Status(), 5000)
     }
-    await checkInferenceEC2Status()
+    await updateOption()
 
-    const url = `http://${ip}:7860/sdapi/v1/txt2img`
+    const url = `http://${instanceIp}:7860${endpoint}`
     console.log('url: ', url)
     const response = await axios.post(url, params)
     console.log('🔴🔴🔴🔴🔴', Object.keys(response.data))
     const { images, parameters, info } = response.data
+    console.log(id)
     let { error } = await supabase
       .from('a1_request')
-      .update({
+      .upsert({
+        id,
         status: 'completed',
         inferenceParameters: parameters,
         inferenceInfo: info,
         resultData: images,
         updatedAt: new Date()
       })
-      .eq('id', id)
+      .select()
+    // .eq('id', id)
     if (error) { throw error }
     res.status(200).send({ message: 'success' });
   } catch (error) {
