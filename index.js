8fb2e426716315a48c21a41fe01b63600d99c29d
yjyjyjy <prowessyang@gmail.com>
2023-03-19 20:36:12 -0700

    works!

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const functions = require('@google-cloud/functions-framework');
-const createClient = require('@supabase/supabase-js').createClient
+const createClient = require('@supabase/supabase-js').createClient;
+const axios = require('axios');
 
 functions.http('sdrequest', async (req, res) => {
-  console.log('YOoooooo!')
-  console.log('🔴', process.env)
-  // const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
-
-
+  console.log(`🪵:sdrequest:${JSON.stringify(req.body)}`)
+  const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
+  const { id, url, params } = req.body
   try {
-    if (req.body.temp < 100) {
-      res.status(200).send("Temperature OK");
-    } else {
-      res.status(200).send(process.env);
-    }
+    const response = await axios.post(url, params)
+    console.log('🔴🔴🔴🔴🔴', Object.keys(response.data))
+    const { images, parameters, info } = response.data
+    let { error } = await supabase
+      .from('a1_request')
+      .update({
+        inferenceParameters: parameters,
+        inferenceInfo: info,
+        resultData: images,
+        updatedAt: new Date()
+      })
+      .eq('id', id)
+    if (error) { throw error }
+    res.status(200).send({ message: 'success' });
   } catch (error) {
     //return an error
     console.log("got error: ", error);
